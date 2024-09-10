6d1fd01c6a3bf23672e332cb04a0b0a226003759
zazzix <rusmaniak@gmail.com>
2024-09-10 18:40:25 +0000

    feat: implement player and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello pups!");
+const choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+    return prompt("Choose: Rock, Paper or Scissors.").toLowerCase();
+}
+
